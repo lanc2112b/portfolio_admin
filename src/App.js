0e1ad00e9612b8497418a5a6069028e2692f89a1
Tony Lancaster <lanc1968@gmail.com>
2023-04-15 13:01:45 +0100

    Add Routes. Add Route / Add Route /register

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import NewAccountPage from './components/auth/NewAccountPage';
 import { Header, Footer, MainSection } from "./components/structure/index";
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="w-full flex flex-col min-h-screen">
       <Header />
-      <MainSection />
+      <Routes>
+        <Route path="/" element={<MainSection />} />
+        <Route path="/register" element={<MainSection element={<NewAccountPage />} />} />
+
+      </Routes>
       <Footer />
     </div>
   );

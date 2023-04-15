a157e1f87dd80be0392618fa499e7ea1c8c65c9f
Tony Lancaster <lanc1968@gmail.com>
2023-04-15 14:02:49 +0100

    Change spinner button to pill type appearance.

--- src/components/uiparts/SpinnerButton.jsx
+++ src/components/uiparts/SpinnerButton.jsx
@@ -1,7 +1,7 @@
 const SpinnerButton = ({buttonText}) => {
     return (
         <>
-            <button type="button" className="bg-indigo-500 rounded-lg py-2 px-3 m-2 font-semibold text-white" disabled>
+            <button type="button" className="bg-indigo-500 rounded-full py-2 px-3 m-2 font-semibold text-white" disabled>
                 <i className="animate-spin fa-solid fa-spinner me-2"></i>
                 <span >{buttonText}</span>
             </button>

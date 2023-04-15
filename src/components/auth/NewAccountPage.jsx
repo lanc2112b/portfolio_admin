78b6e894496bfe3e33f06c3a12ccc15adc14c6f2
Tony Lancaster <lanc1968@gmail.com>
2023-04-15 14:18:39 +0100

    Change h2 text.

--- src/components/auth/NewAccountPage.jsx
+++ src/components/auth/NewAccountPage.jsx
@@ -5,7 +5,7 @@ const NewAccountPage = () => {
     return (
         <>
             <section id="registration-page" className="w-full flex flex-col grow items-center p-8">
-                <h2 className="my-4 font-semibold text-lg text-zinc-600">Register Now</h2>
+                <h2 className="my-4 font-semibold text-lg text-zinc-600">Register</h2>
                 <hr className="w-60 mx-auto border-zinc-500 border-1 mb-4" />
                 <NewAccountButton />
                 <ul className="mt-6 fa-ul">

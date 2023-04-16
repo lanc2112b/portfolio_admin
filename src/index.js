20299feb5a599ea5e72e0450835df86e8ec738fc
Tony Lancaster <lanc1968@gmail.com>
2023-04-16 14:17:34 +0100

    Add MessageProvider (message context).

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { UserProvider } from "./contexts/User";
+import { MessageProvider } from "./contexts/Message";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

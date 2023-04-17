94c446a856ac29734703ccbcc831f942ee70e7f7
Tony Lancaster <lanc1968@gmail.com>
2023-04-17 12:57:00 +0100

    Import luxon for easy date formatting. Add onClick modalHandler.

--- src/components/contacts/ContactItemRow.jsx
+++ src/components/contacts/ContactItemRow.jsx
@@ -1,6 +1,7 @@
 //import { useState } from "react";
+import { DateTime } from 'luxon';
 
-const ContactItemRow = ({ element }) => {
+const ContactItemRow = ({ element, modalHandler }) => {
 
     //const { show, setShow } = useState(true);
 
@@ -8,12 +9,14 @@ const ContactItemRow = ({ element }) => {
         <>
             <td className="px-3 py-2">{element.name}</td>
             <td className="px-3 py-2">{element.email}</td>
-            <td className="px-3 py-2">{element.subject}</td>
-            <td className="px-3 py-2">{element.query}</td>
-            <td className="px-3 py-2">{element.created_at}</td>
+            <td className="px-3 py-2 hidden md:table-cell">{element.subject}</td>
+            <td className="px-3 py-2 hidden md:table-cell">{element.query}</td>
+            <td className="px-3 py-2">
+                {DateTime.fromSQL(element.created_at).toLocaleString(DateTime.DATE)}
+            </td>
             <td className="px-3 py-2 text-center">{element.notified ? <i className="fa-regular fa-circle-check text-lime-600"></i> : <i className="fa-regular fa-circle-xmark text-orange-600"></i>}</td>
             <td className="px-3 py-2 text-center">
-                <button type="button" value={element.id} className="hover:bg-cyan-100 text-sky-700 py-1 px-3 rounded-lg">
+                <button type="button" value={element.id} onClick={() => modalHandler(element.id)} className="hover:bg-cyan-100 text-sky-700 py-1 px-3 rounded-lg">
                     <i className="fa-solid fa-expand"></i>
                 </button>
             </td>

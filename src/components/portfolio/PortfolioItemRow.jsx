b47a7df1138c67bea2ccc389393aaab2c74e61f4
Tony Lancaster <lanc1968@gmail.com>
2023-04-17 22:42:14 +0100

    Add portfolio items table Add portfolio table row component

---
+++ src/components/portfolio/PortfolioItemRow.jsx
@@ -0,0 +1,27 @@
+import { DateTime } from 'luxon';
+
+const PortfolioItemRow = ({ element }) => {
+
+    return (
+        <>
+            <td className="px-3 py-2">{element.title}</td>
+            <td className="px-3 py-2">{element.hosted_url}</td>
+            <td className="px-3 py-2">{element.github_url}</td>
+            <td className="px-3 py-2">
+                {DateTime.fromSQL(element.created_at).toLocaleString(DateTime.DATE)}
+            </td>
+            <td className="px-3 py-2 text-center">
+                <button type="button" value={element.id} className="hover:bg-orange-200 text-orange-700 py-1 px-3 rounded-lg">
+                    <i className="fa-solid fa-pen"></i>
+                </button>
+            </td>
+            <td className="px-3 py-2 text-center">
+                <button type="button" value={element.id} className="hover:bg-cyan-100 text-sky-700 py-1 px-3 rounded-lg">
+                    <i className="fa-solid fa-expand"></i>
+                </button>
+            </td>
+        </>
+    )
+}
+
+export default PortfolioItemRow;

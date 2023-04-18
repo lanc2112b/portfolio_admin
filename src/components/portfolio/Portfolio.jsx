47b4410e64cf29e53a75ff249ff605fa5c3892a4
Tony Lancaster <lanc1968@gmail.com>
2023-04-18 20:54:43 +0100

    Refactor toggle expanded. Move button & handler up a level from child component.

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -11,6 +11,7 @@ const Portfolio = () => {
 
     const [list, setList] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [expanded, setExpanded] = useState(false);
 
     useEffect(() => {
         setLoading(true);
@@ -26,12 +27,24 @@ const Portfolio = () => {
 
     }, [user.access_token]);
 
+    const toggleExpanded = () => {
+
+        setExpanded(!expanded);
+
+    }
+
     if (loading)
         return (<><button type="button" className="text-2xl rounded-full text-cyan-100 bg-slate-500 py-3 px-4" disabled><i className="animate-spin fa-solid fa-spinner me-3"></i> Loading... </button></>)
 
     return (
         <>
-            <PortfolioItemsList list={list} />
+            <div className="w-full flex justify-end py-2 px-3">
+                <button type="button" className="bg-green-500 text-white font-semibold rounded-full focus:bg-green-600 hover:bg-green-600 px-3 py-1 shadow-md" onClick={toggleExpanded}>
+                    {!expanded ? <i className="me-3 fa-regular fa-square-plus"></i> : <i className="me-3 fa-regular fa-square-minus"></i>}
+                    Add
+                </button>
+            </div>
+            <PortfolioItemsList list={list} expanded={expanded} />
         </>
     );
 }

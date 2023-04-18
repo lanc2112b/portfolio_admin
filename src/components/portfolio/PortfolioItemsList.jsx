9b89e626d0f615346cbf1208ded037830672c0e4
Tony Lancaster <lanc1968@gmail.com>
2023-04-18 22:19:28 +0100

    pass down deleteHandler prop. Add column for delete action / icons.

--- src/components/portfolio/PortfolioItemsList.jsx
+++ src/components/portfolio/PortfolioItemsList.jsx
@@ -1,7 +1,7 @@
 import PortfolioForm from "./PortfolioForm";
 import PortfolioItemRow from "./PortfolioItemRow";
 
-const PortfolioItemsList = ({ list, expanded }) => {
+const PortfolioItemsList = ({ list, expanded, deleteHandler }) => {
 
     return (
         <>
@@ -15,6 +15,7 @@ const PortfolioItemsList = ({ list, expanded }) => {
                             <th className="text-left p-3">GitHub URL</th>
                             <th className="text-left p-3">Date</th>
                             <th className="text-center text-xl p-3"><i className="fa-solid fa-file-pen"></i></th>
+                            <th className="text-center text-xl p-3"><i className="fa-solid fa-trash"></i></th>
                         </tr>
                     </thead>
                     <tbody>
@@ -22,7 +23,7 @@ const PortfolioItemsList = ({ list, expanded }) => {
                         {list.map((element) => {
                             return (
                                 <tr key={element.id} className=" border-slate-600 border-y even:bg-slate-100 odd:bg-white">
-                                    <PortfolioItemRow element={element} />
+                                    <PortfolioItemRow element={element} deleteHandler={deleteHandler} />
                                 </tr>
                             )
                         })}

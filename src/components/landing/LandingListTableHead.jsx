4c5625f9c1a28291d040e0bb12e828a5469f0f48
Tony Lancaster <lanc1968@gmail.com>
2023-04-20 11:17:28 +0100

    Add LandingListTableHead component

---
+++ src/components/landing/LandingListTableHead.jsx
@@ -0,0 +1,17 @@
+const LandingListTableHead = () => {
+
+    return (
+        <>
+            <thead>
+                <tr className="bg-slate-300">
+                    <th className="text-left p-3">Area Title: </th>
+                    <th className="text-left p-3">Content Title</th>
+                    <th className="text-center text-xl p-3"><i className="fa-solid fa-file-pen"></i></th>
+                </tr>
+            </thead>
+        </>
+    )
+
+}
+
+export default LandingListTableHead;

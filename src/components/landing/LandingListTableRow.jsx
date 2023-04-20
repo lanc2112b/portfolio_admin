4bec5e3bac73fc996fc6854fb916513e96508be9
Tony Lancaster <lanc1968@gmail.com>
2023-04-20 16:24:23 +0100

    Add prop/arg deleteModalHandler. Add column for delete action icons.

--- src/components/landing/LandingListTableRow.jsx
+++ src/components/landing/LandingListTableRow.jsx
@@ -1,4 +1,4 @@
-const LandingListTableRow = ({ element }) => {
+const LandingListTableRow = ({ element, deleteModalHandler }) => {
 
     const editClickHandler = (value) => {
 
@@ -12,6 +12,11 @@ const LandingListTableRow = ({ element }) => {
             <td className="px-3 py-2 text-center">
                 <button type="button" value={element.id} className="hover:bg-orange-200 text-orange-700 py-1 px-3 rounded-lg" onClick={() => editClickHandler(element.id)}>
                     <i className="fa-solid fa-pen"></i>
+                </button>
+            </td>
+            <td className="px-3 py-2 text-center">
+                <button type="button" value={element.id} className="hover:bg-red-100 text-red-700 py-1 px-3 rounded-lg" onClick={() => deleteModalHandler(element.id)}>
+                    <i className="fa-solid fa-folder-minus"></i>
                 </button>
             </td>
         </>

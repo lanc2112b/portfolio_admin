66941753e01bea4b29d889a7591c6bca88f6d242
Tony Lancaster <lanc1968@gmail.com>
2023-04-20 14:25:42 +0100

    Add expand form button. Aff form component. Add form expanded handler / state. Add setListHandler to update list on submit (optimistic render). Fix, key unique on correct element.

--- src/components/landing/LandingPage.jsx
+++ src/components/landing/LandingPage.jsx
@@ -2,11 +2,13 @@ import { useState, useEffect } from "react";
 import { getLandingPageItems } from "../../api/ApiConsumer";
 import LandingListTableHead from "./LandingListTableHead";
 import LandingListTableRow from "./LandingListTableRow";
+import LandingContentForm from "./LandingContentForm";
 
 const LandingPage = () => {
 
     const [areaList, setAreaList] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [expanded, setExpanded] = useState(false);
 
     useEffect(() => {
 
@@ -24,6 +26,16 @@ const LandingPage = () => {
 
     }, [])
 
+    const setListHandler = (result) => {
+        setAreaList((currentList) => [...currentList, result]);
+    };
+
+    const toggleExpanded = () => {
+
+        setExpanded(!expanded);
+
+    }
+
     const editClickHandler = (value) => {
         console.log(value);
     }
@@ -35,12 +47,20 @@ const LandingPage = () => {
     return (
         <>
             <section id="list-landingpage-content">
+
+                <div className="w-full flex justify-end py-2 px-3">
+                    <button type="button" className="bg-green-500 text-white font-semibold rounded-full focus:bg-green-600 hover:bg-green-600 px-3 py-1 shadow-md" onClick={toggleExpanded}>
+                        {!expanded ? <i className="me-3 fa-regular fa-square-plus"></i> : <i className="me-3 fa-regular fa-square-minus"></i>}
+                        Add
+                    </button>
+                </div>
+                <LandingContentForm expanded={expanded} formMode={'add'} setListHandler={setListHandler} />
                 <div className="w-full shadow-md">
                     <table className="w-full border-separate border-spacing-y-1">
                         <LandingListTableHead />
                         <tbody>
                             {areaList.map((element) => {
-                                return (<> <tr key={element.id}><LandingListTableRow element={element} editClickHandler={editClickHandler} /> </tr> </>)
+                                return (<tr key={element.id}><LandingListTableRow element={element} editClickHandler={editClickHandler} /></tr>)
                             })}
                         </tbody>
                     </table>

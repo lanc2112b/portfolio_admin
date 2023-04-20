5d7b32573944985c314ccb447475ceb60809d687
Tony Lancaster <lanc1968@gmail.com>
2023-04-20 16:22:02 +0100

    Add deleteModalHandler. Add deleteHandler. Add confirmation modal.

--- src/components/landing/LandingPage.jsx
+++ src/components/landing/LandingPage.jsx
@@ -1,14 +1,19 @@
-import { useState, useEffect } from "react";
-import { getLandingPageItems } from "../../api/ApiConsumer";
+import { useState, useEffect, useContext } from "react";
+import { UserContext } from "../../contexts/User";
+import { getLandingPageItems, deleteLandingItem } from "../../api/ApiConsumer";
 import LandingListTableHead from "./LandingListTableHead";
 import LandingListTableRow from "./LandingListTableRow";
 import LandingContentForm from "./LandingContentForm";
 
 const LandingPage = () => {
 
+    const { user } = useContext(UserContext);
+
     const [areaList, setAreaList] = useState([]);
     const [loading, setLoading] = useState(false);
     const [expanded, setExpanded] = useState(false);
+    const [showModal, setShowModal] = useState(false);
+    const [deleteId, setDeleteId] = useState(null);
 
     useEffect(() => {
 
@@ -36,6 +41,37 @@ const LandingPage = () => {
 
     }
 
+    const deleteModalHandler = (value) => {
+        setShowModal(true);
+        setDeleteId(value);
+    }
+
+    const deleteHandler = (value) => {
+        //console.log(value, deleteId, 'values match?');
+        //do delete: 
+        deleteLandingItem(user.access_token, value)
+            .then((result) => {
+                if (result === 204) {
+                    //setList();
+                    const filtered = areaList.filter((element) => {
+                        return element.id !== value;
+                    });
+
+                    setAreaList(filtered);
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        setShowModal(false);
+        setDeleteId(null);
+
+    }
+
+    const closeModalHandler = () => {
+        setShowModal(false);
+    }
+
     if (loading)
         return (<> <p><button > loading... </button></p></>);
 
@@ -56,13 +92,57 @@ const LandingPage = () => {
                         <LandingListTableHead />
                         <tbody>
                             {areaList.map((element) => {
-                                return (<tr key={element.id}><LandingListTableRow element={element} /></tr>)
+                                return (<tr key={element.id}><LandingListTableRow element={element} deleteModalHandler={deleteModalHandler} /></tr>)
                             })}
                         </tbody>
                     </table>
                 </div>
                 {/**  Add content / FP preview here. Demonstrates which section is to be edited */}
             </section>
+
+            <div className={`${showModal ? '' : 'hidden'} fixed inset-0 z-50 justify-center items-center flex overflow-x-hidden overflow-y-auto outline-none focus:outline-none mx-1`}>
+                <div className="relative w-full my-6 mx-auto max-w-xl">
+                    {/*content*/}
+                    <div className="border-0 rounded-lg p-5 shadow-lg relative flex flex-col w-full bg-zinc-50 outline-none focus:outline-none">
+                        {/*header*/}
+                        <div className="flex items-start justify-between pb-3 border-b border-solid border-zinc-800 rounded-t">
+                            <h3 className="text-2xl font-semibold text-red-600">
+                                <i className="fa-solid fa-hand me-4"></i> Stop!
+                            </h3>
+                            <button
+                                className="ml-auto bg-transparent border-0 text-slate-50 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                                onClick={() => setShowModal(false)}
+                            >
+                                <span className="bg-transparent text-zinc-800 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                                    ×
+                                </span>
+                            </button>
+                        </div>
+                        {/*body*/}
+                        <div className="relative p-6 flex-auto">
+                            Are you sure you want to delete this area content item? This action cannot be reversed.
+                        </div>
+                        {/*footer*/}
+                        <div className="flex items-center justify-end p-5 pe-0 border-t border-solid border-zinc-800 rounded-b">
+                            <button
+                                className=" text-zinc-800 font-bold uppercase text-sm px-3 py-1 outline-none focus:outline-none me-3 mb-1"
+                                type="button"
+                                onClick={closeModalHandler}
+                            >
+                                Close
+                            </button>
+                            <button
+                                className="text-slate-50 bg-red-500 active:bg-red-600 hover:bg-red-600 font-bold uppercase text-sm px-3 py-1 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                type="button"
+                                onClick={() => deleteHandler(deleteId)}
+                            >
+                                Yes, Delete!
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={`${showModal ? '' : 'hidden'} opacity-25 fixed inset-0 z-40 bg-black`}></div>
         </>
     )
 

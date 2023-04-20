731ef54459948a6625a57f190060f1182b44199d
Tony Lancaster <lanc1968@gmail.com>
2023-04-20 21:38:17 +0100

    Fix table column widths.

--- src/components/contacts/ContactItemRow.jsx
+++ src/components/contacts/ContactItemRow.jsx
@@ -7,15 +7,15 @@ const ContactItemRow = ({ element, modalHandler }) => {
 
     return (
         <>
-            <td className="px-3 py-2">{element.name}</td>
+            <td className="px-3 py-2 hidden sm:table-cell">{element.name}</td>
             <td className="px-3 py-2">{element.email}</td>
             <td className="px-3 py-2 hidden md:table-cell">{element.subject}</td>
-            <td className="px-3 py-2 hidden md:table-cell">{element.query}</td>
+            <td className="px-3 py-2 hidden xl:table-cell">{element.query}</td>
             <td className="px-3 py-2">
                 {DateTime.fromSQL(element.created_at).toLocaleString(DateTime.DATE)}
             </td>
-            <td className="px-3 py-2 text-center">{element.notified ? <i className="fa-regular fa-circle-check text-lime-600"></i> : <i className="fa-regular fa-circle-xmark text-orange-600"></i>}</td>
-            <td className="px-3 py-2 text-center">
+            <td className="px-3 py-2 text-center w-1/12">{element.notified ? <i className="fa-regular fa-circle-check text-lime-600"></i> : <i className="fa-regular fa-circle-xmark text-orange-600"></i>}</td>
+            <td className="px-3 py-2 text-center w-1/12">
                 <button type="button" value={element.id} onClick={() => modalHandler(element.id)} className="hover:bg-cyan-100 text-sky-700 py-1 px-3 rounded-lg">
                     <i className="fa-solid fa-expand"></i>
                 </button>

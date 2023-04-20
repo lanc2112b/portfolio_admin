92edfd2c346d82e275db179a1e368bd9dcbdd9fe
Tony Lancaster <lanc1968@gmail.com>
2023-04-20 15:53:26 +0100

    Add editClickHandler callback.

--- src/components/landing/LandingListTableRow.jsx
+++ src/components/landing/LandingListTableRow.jsx
@@ -1,4 +1,10 @@
-const LandingListTableRow = ({ element, editClickHandler }) => {
+const LandingListTableRow = ({ element }) => {
+
+    const editClickHandler = (value) => {
+
+        window.location.replace(`/landing-content/${value}`);
+
+    }
     return (
         <>
             <td className="px-3 py-2">{element.area_title}</td>

846166038cbc9aeffd6218c5d9fe3056ab717075
Tony Lancaster <lanc1968@gmail.com>
2023-04-21 16:42:36 +0100

    Add Toaster - shows toast notifications. Uses Bread component.

--- src/components/structure/Header.jsx
+++ src/components/structure/Header.jsx
@@ -1,3 +1,4 @@
+import { Toaster } from 'react-hot-toast';
 import HeaderNav from '../navs/HeaderNav';
 
 const Header = () => {
@@ -5,10 +6,22 @@ const Header = () => {
   return (
     <>
       <div className="w-full border-b md:ps-2">
-      <header className="w-full container mx-auto md:flex md:justify-between md:items-center md:px-2 md:py-3">
-        <HeaderNav />
+        <header className="w-full container mx-auto md:flex md:justify-between md:items-center md:px-2 md:py-3">
+          <HeaderNav />
         </header>
       </div>
+      <Toaster
+        position="top-right"
+        reverseOrder={false}
+        gutter={8}
+        containerClassName=""
+        containerStyle={{}}
+        toastOptions={{
+          // Define default options
+          className: 'w-60 shadow-green-300 bg-green-200',
+          duration: 8000,
+        }}
+      />
     </>
   )
 

dd63b3577d238dff08226fddabc0cf87a342a615
Tony Lancaster <lanc1968@gmail.com>
2023-04-21 11:37:13 +0100

    Add ErrorMessage component. Implement ErrorMessage in MainSection.

--- src/components/structure/MainSection.jsx
+++ src/components/structure/MainSection.jsx
@@ -1,10 +1,13 @@
-const MainSection = ({element}) => {
+import ErrorMessage from "../uiparts/ErrorMessage";
+const MainSection = ({ element }) => {
 
   return (
     <>
       <main className="container mt-3 mb-6 mx-auto px-2 flex flex-col grow">
+        <ErrorMessage />
           {element}
       </main>
+
     </>
   )
 

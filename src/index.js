47659532c0aab302243c83ccf194844bd9897479
Tony Lancaster <lanc1968@gmail.com>
2023-05-27 12:29:53 +0100

    Add GoogleOAuthProvider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import './index.css';
 import App from './App';
 import { UserProvider } from "./contexts/User";
@@ -9,12 +10,14 @@ import { MessageProvider } from "./contexts/Message";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-    <BrowserRouter>
+  <BrowserRouter>
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
       <MessageProvider>
         <UserProvider>
           <App />
         </UserProvider>
       </MessageProvider>
-    </BrowserRouter>
+    </GoogleOAuthProvider>
+  </BrowserRouter>
 
 );

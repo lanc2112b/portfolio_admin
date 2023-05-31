c81ad37f47638c2b76b9876024c5f69c6a2652d2
Tony Lancaster <lanc1968@gmail.com>
2023-05-31 17:13:58 +0100

    refactor MainSection component

--- src/components/structure/MainSection.jsx
+++ src/components/structure/MainSection.jsx
@@ -1,13 +1,19 @@
+import { Outlet } from "react-router-dom";
 import ErrorMessage from "../uiparts/ErrorMessage";
-const MainSection = ({ element }) => {
+import { Header, Footer } from "../structure/index";
+
+const MainSection = () => {
 
   return (
     <>
-      <main className="container mt-3 mb-6 mx-auto px-2 flex flex-col grow">
-        <ErrorMessage />
-          {element}
-      </main>
-
+      <div id="wrapper" className="w-full flex flex-col min-h-screen">
+        <Header />
+        <main className="container mt-3 mb-6 mx-auto px-2 flex flex-col grow">
+          <ErrorMessage />
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
     </>
   )
 

e16c2a76139eefb351841d8236b66fd27c5348e4
Tony Lancaster <lanc1968@gmail.com>
2023-05-31 17:04:28 +0100

    refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import './index.css';
-import App from './App';
+
 import { UserProvider } from "./contexts/User";
 import { MessageProvider } from "./contexts/Message";
 
+import App from './App';
+import './index.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
-  <BrowserRouter>
-    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
-      <MessageProvider>
-        <UserProvider>
-          <App />
-        </UserProvider>
-      </MessageProvider>
-    </GoogleOAuthProvider>
-  </BrowserRouter>
-
+    <BrowserRouter>
+      <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+        <MessageProvider>
+          <UserProvider>
+            <Routes>
+              <Route path="/*" element={<App />} />
+            </Routes>
+          </UserProvider>
+        </MessageProvider>
+      </GoogleOAuthProvider>
+    </BrowserRouter>
 );
